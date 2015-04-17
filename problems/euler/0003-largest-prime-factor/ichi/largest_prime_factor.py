4a4533da79fa89c1367e471aa2b1daccfa1c5a62
Craig Ferry <craig@puritycontrol.net>
2015-04-17 21:15:31 +0100

    working implementation too slow to calculate the euler problem
    - need to succesively reduct the factor by find the lower prime factors
      and dividing the factor by these numbers

--- problems/euler/0003-largest-prime-factor/ichi/largest_prime_factor.py
+++ problems/euler/0003-largest-prime-factor/ichi/largest_prime_factor.py
@@ -1,6 +1,9 @@
 def get(factor):
-    return
+    return max(x for x in up_to(factor, primes()) if factor_p(factor,x))
 
+
+def factor_p(factor, divisor):
+    return factor % divisor == 0
 
 def get_kval_primes(k_val, primes):
     #filter primes only from 1+kval and 1-kval
@@ -16,6 +19,23 @@ def prime_p(num, primes):
 
 def at_end_of_primes(counter, primes):
     return counter == len(primes) - 1
+
+
+def up_to(num, seq):
+    """Generator given num yields all items from the sequence until
+    num is exceeded
+
+    Args:
+      num: number that represents a limit the generator will stop
+      at when exceeded
+
+      seq: the sequence to iterate over
+    """
+    for item in seq:
+        if item <= num:
+            yield item
+        else:
+            return
 
 def primes():
     counter = 0

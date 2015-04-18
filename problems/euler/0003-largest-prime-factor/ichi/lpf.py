6da5fe80948cbd9f08e567f2ac9e33e5fc822d84
Craig Ferry <craig@puritycontrol.net>
2015-04-18 08:36:14 +0100

    refactor use _ convention for private methods

--- problems/euler/0003-largest-prime-factor/ichi/lpf.py
+++ problems/euler/0003-largest-prime-factor/ichi/lpf.py
@@ -7,31 +7,31 @@ def largest_prime_factor(dividend):
       prime factor for
     """
     factors = []
-    while product_of_factors(factors) < dividend:
+    while _product_of_factors(factors) < dividend:
         factors.append(
-          lowest_prime_factor(reduced_dividend(dividend, factors)))
+          _lowest_prime_factor(_reduced_dividend(dividend, factors)))
     return max(factors)
 
 
-def reduced_dividend(dividend, factors):
+def _reduced_dividend(dividend, factors):
     """The new dividend as a result of dividing the original
     dividend by the list of prime factors calculated so far."""
-    return dividend/product_of_factors(factors)
+    return dividend/_product_of_factors(factors)
 
 
-def product_of_factors(seq):
+def _product_of_factors(seq):
     """return the product of numbers in the list or 1 for the
     empty list to prevent division by zero errors"""
     return reduce(lambda x, y: x*y, seq, 1)
 
 
-def lowest_prime_factor(factor):
+def _lowest_prime_factor(factor):
     for prime in primes():
-        if factor_p(factor, prime):
+        if _factor_p(factor, prime):
             return prime
 
 
-def factor_p(factor, divisor):
+def _factor_p(factor, divisor):
     return factor % divisor == 0
 
 
@@ -55,28 +55,26 @@ def primes():
     """ Geneartor that lazily evaluates the next prime number.
 
     Warning: This will produce an infinte sequence"""
-    def end_of_primes_p(counter, primes):
+    def _end_of_primes_p(counter, primes):
         return counter == len(primes) - 1
 
-    def prime_p(num, primes):
-        #filter primes smaller than num and see if they are factors
+    def _prime_p(num, primes):
         for prime in primes:
             if num % prime == 0:
                 return False
         return True
 
 
-    def get_kval_primes(k_val, primes):
-        #filter primes only from 1+kval and 1-kval
-        return [x for x in [k_val*6-1, k_val*6+1] if  prime_p(x, primes)]
+    def _get_kval_primes(k_val, primes):
+        return [x for x in [k_val*6-1, k_val*6+1] if  _prime_p(x, primes)]
 
 
     counter = 0
     primes = [2, 3]
     k_val = 1
     while True:
-        while end_of_primes_p(counter, primes):
-            next_primes = get_kval_primes(k_val, list(primes))
+        while _end_of_primes_p(counter, primes):
+            next_primes = _get_kval_primes(k_val, list(primes))
             k_val += 1
             for prime in next_primes:
                 primes.append(prime)
--- problems/euler/0003-largest-prime-factor/ichi/lpf_test.py
+++ problems/euler/0003-largest-prime-factor/ichi/lpf_test.py
@@ -6,7 +6,7 @@ import lpf
 
 class LargestPrimeFactorTests(unittest.TestCase):
 
-    def test_even_fibs_to_4000000(self):
+    def test_lpf_for_euler(self):
         self.assertEqual(
             6857,
             lpf.largest_prime_factor(600851475143)

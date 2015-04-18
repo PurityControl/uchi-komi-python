f2bb7e3a18f44c4de0a0ef66acf990b1a474f54c
Craig Ferry <craig@puritycontrol.net>
2015-04-18 08:04:08 +0100

    refactor prime helper functions
    - rename at_end_or_primes to lisp style end_of_prime_p to match
      style of other predicates
    - move all helper functions into prime generator functionas nested
      functions

--- problems/euler/0003-largest-prime-factor/ichi/lpf.py
+++ problems/euler/0003-largest-prime-factor/ichi/lpf.py
@@ -35,22 +35,6 @@ def factor_p(factor, divisor):
     return factor % divisor == 0
 
 
-def get_kval_primes(k_val, primes):
-    #filter primes only from 1+kval and 1-kval
-    return [x for x in [k_val*6-1, k_val*6+1] if  prime_p(x, primes)]
-
-
-def prime_p(num, primes):
-    #filter primes smaller than num and see if they are factors
-    for prime in primes:
-        if num % prime == 0:
-            return False
-    return True
-
-def at_end_of_primes(counter, primes):
-    return counter == len(primes) - 1
-
-
 def up_to(num, seq):
     """Generator given num yields all items from the sequence until
     num is exceeded
@@ -68,11 +52,30 @@ def up_to(num, seq):
             return
 
 def primes():
+    """ Geneartor that lazily evaluates the next prime number.
+
+    Warning: This will produce an infinte sequence"""
+    def end_of_primes_p(counter, primes):
+        return counter == len(primes) - 1
+
+    def prime_p(num, primes):
+        #filter primes smaller than num and see if they are factors
+        for prime in primes:
+            if num % prime == 0:
+                return False
+        return True
+
+
+    def get_kval_primes(k_val, primes):
+        #filter primes only from 1+kval and 1-kval
+        return [x for x in [k_val*6-1, k_val*6+1] if  prime_p(x, primes)]
+
+
     counter = 0
     primes = [2, 3]
     k_val = 1
     while True:
-        while at_end_of_primes(counter, primes):
+        while end_of_primes_p(counter, primes):
             next_primes = get_kval_primes(k_val, list(primes))
             k_val += 1
             for prime in next_primes:

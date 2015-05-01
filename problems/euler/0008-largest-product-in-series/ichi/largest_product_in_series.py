382849af0dc23d0cac3446f60c6c4efe37c47d4a
Craig Ferry <craig@puritycontrol.net>
2015-05-01 11:42:32 +0100

    changed method to use list comprehension in favour of a loop

--- problems/euler/0008-largest-product-in-series/ichi/largest_product_in_series.py
+++ problems/euler/0008-largest-product-in-series/ichi/largest_product_in_series.py
@@ -6,12 +6,10 @@ def largest_product(length, str_of_digits):
       length: the length of contiguous digits to be calculated
       str_of_digits: the string of digits to calculate the products from
     """
-    max = 0
-    for nums in succesive_seqs(length, str_of_digits):
-        product = reduce(lambda x,y: x*y, nums)
-        if product > max:
-          max = product
-    return max
+    return max(product(nums) for nums in succesive_seqs(length, str_of_digits))
+
+def product(lodigits):
+    return reduce(lambda x,y: x*y, lodigits)
 
 
 def succesive_seqs(length, str_of_digits):
